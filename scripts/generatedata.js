f88d994046c9ae40abb31623f6ef8aa71ef5757c
adityakahb <aditya.bhakay@live.com>
2020-09-18 23:13:45 +0530

    some update

--- scripts/generatedata.js
+++ scripts/generatedata.js
@@ -1,8 +1,23 @@
-const less = require('gulp-less');
-const path = require('path');
 const fs = require('fs');
-const gulp = require('gulp');
-const sourcemaps = require('gulp-sourcemaps');
+
+const htmltop = `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>AMegMen</title>
+  <link rel="stylesheet" href="./dist/styles/index.css">
+</head>
+<body>`;
+
+const htmlbottom = `
+    <script src="./dist/scripts/index.js"></script>
+    <script>
+      var mm_instance1 = AMegMen.Root.getRoot();
+      mm_instance1.init('.__amegmen');
+    </script>
+  </body>
+</html>`;
 
 const mainStr =
   'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ' +
@@ -56,7 +71,7 @@ mainArr = mainStr.replace(/^\s+|\s+$/g, '').toLowerCase().split('. ').join(' ').
 
 const genName = (length) => {
   let genarr = [];
-  for (let i=0; i<length; i++) {
+  for (let i = 0; i < length; i++) {
     genarr.push(mainArr[Math.floor(Math.random() * mainArr.length)]);
   }
   return genCap(genarr.join(' '));
@@ -64,8 +79,23 @@ const genName = (length) => {
 
 const genPageMenu = (len, count) => {
   let navlength = len;
-  let mainnavstr = '<nav class="__c--amegmen" id="_c_amegmen' + count + '"><ul>'
-  for(let i = 0; i < navlength; i++) {
+  let mainnavstr = '<nav class="__amegmen">';
+  if (count === 2) {
+    mainnavstr = '<nav class="__amegmen" id="__amegmen_123">';
+  }
+  
+  mainnavstr += `<button class="__amegmen--toggle-cta">
+    Menu
+  </button>`;
+  mainnavstr += `<div class="__amegmen--canvas">
+  <header>
+    <button class="__amegmen--close-cta">
+      Close
+    </button>
+    </header>
+    <section class="__amegmen--main">
+  <ul>`;
+  for (let i = 0; i < navlength; i++) {
     let link0Str = '<li><a href="#">'
     link0Str += genName(randomNum(1, 2)) + '</a>';
 
@@ -73,42 +103,44 @@ const genPageMenu = (len, count) => {
     let hasSubnav = 1;
 
     if (hasSubnav === 1) {
-      let subnav0str = '<section>'
-      let landing0link = '<div class="__c--amegmen-landing"><a href="#">Landing page: ' + genName(randomNum(3, 6)) + '</a></div>';
+      let subnav0str = '<section class="__amegmen--panel">';
+      let landing0link = `<div class="__amegmen--landing">
+      <button class="__amegmen--main-cta">Main</button>
+      <a href="#">Landing page: ` + genName(randomNum(3, 6)) + `</a></div>`;
       subnav0str += landing0link;
       // let hasSubnav1 = randomNum(1, 2);
       let hasSubnav1 = 1;
       let subnav1str = '';
       if (hasSubnav1 === 1) {
-        let columns1 = randomNum(1, 4);
-        subnav1str = '<nav class="__c--amegmen-subnav">';
-        for (let j=0; j < columns1; j++) {
-          subnav1str += '<div class="__c--amegmen-col"><div class="__c--amegmen-col-spacer">';
-          
+        let columns1 = randomNum(1, 2);
+        subnav1str = '<nav>';
+        for (let j = 0; j < columns1; j++) {
+          subnav1str += '<div class="__amegmen--col">';
+
           let subnav1len = randomNum(5, 10);
           let subnavlinks1str = '<ul>';
 
-          for (let k=0; k < subnav1len; k++) {
+          for (let k = 0; k < subnav1len; k++) {
             subnavlinks1str += '<li><a href="#">' + genName(randomNum(3, 6)) + '</a>';
 
             // let hasSubnav2 = randomNum(1, 2);
             let hasSubnav2 = 1;
             if (hasSubnav2 === 1) {
-              let subnav2str = '<section>';
-              let landing2link = '<div class="__c--amegmen-landing"><a href="#">Landing page: ' + genName(randomNum(3, 6)) + '</a></div>';
+              let subnav2str = '<section class="__amegmen--panel">';
+              let landing2link = '<div class="__amegmen--landing"><button class="__amegmen--back-cta">Back</button><a href="#">Landing page: ' + genName(randomNum(3, 6)) + '</a></div>';
               subnav2str += landing2link;
 
-              let columns2 = 5 - columns1;
-              subnav2str += '<nav class="__c--amegmen-subnav">';
-              for (let l=0; l < columns2; l++) {
-                subnav2str += '<div class="__c--amegmen-col"><div class="__c--amegmen-col-spacer"><ul>';
-                let subnav2len = randomNum(5, 8);
+              let columns2 = 1;
+              subnav2str += '<nav>';
+              for (let l = 0; l < columns2; l++) {
+                subnav2str += '<div class="__amegmen--col"><ul>';
+                let subnav2len = randomNum(5, 25);
 
-                for (let m=0; m < subnav2len; m++) {
+                for (let m = 0; m < subnav2len; m++) {
                   subnav2str += '<li><a href="#">' + genName(randomNum(3, 6)) + '</a></li>';
                 }
 
-                subnav2str += '</ul></div></div>';
+                subnav2str += '</ul></div>';
               }
               subnav2str += '</nav></section>';
               subnavlinks1str += subnav2str;
@@ -118,8 +150,9 @@ const genPageMenu = (len, count) => {
           }
 
           subnavlinks1str += '</ul>';
-          subnav1str += subnavlinks1str + '</div></div>';
+          subnav1str += subnavlinks1str + '</div>';
         }
+        subnav1str += '<div class="__amegmen--col">blank column</div>';
         subnav1str += '</nav>';
       }
 
@@ -129,32 +162,21 @@ const genPageMenu = (len, count) => {
 
     mainnavstr += link0Str + '</li>';
   }
-  mainnavstr += '</ul></nav>';
+  mainnavstr += '</ul></section></div></nav>';
 
   return mainnavstr;
 };
 
-gulp.task('less', async function () {
-  gulp.src(['./clientlibs/less/**/*.less', '!./clientlibs/less/**/_*.less'])
-  .pipe(sourcemaps.init())
-  .pipe(less({
-    paths: [ path.join(__dirname, 'less', 'includes') ]
-  }))
-  .pipe(sourcemaps.write())
-  .pipe(gulp.dest('./clientlibs/styles'));
-});
-
-gulp.task('generatedata', async function () {
+const generateData = () => {
   let m1 = genPageMenu(8, 1);
   let m2 = genPageMenu(4, 2);
+  let m3 = genPageMenu(3, 3);
+  let m4 = genPageMenu(1, 4);
   
-  fs.writeFile('./components/megamenu.html', m1, function (err) {
+  fs.writeFile('./index.html', htmltop + m1 + m2 + m3 + m4 + htmlbottom, function (err) {
     if (err) throw err;
-    console.log('AMegMen 1 Replaced!');
+    console.log('Menu Replaced!');
   });
+};
 
-  fs.writeFile('./components/megamenu2.html', m2, function (err) {
-    if (err) throw err;
-    console.log('AMegMen 2 Replaced!');
-  });
-});
+generateData();
