e317452232cc144e3edc744a612eb2c4bea534d7
adityakahb <aditya.bhakay@live.com>
2022-02-22 11:20:13 +0530

    rewrite

--- src/ts/amegmen.ts
+++ src/ts/amegmen.ts
@@ -203,66 +203,6 @@ namespace AMegMen {
   let windowResizeAny: any;
   let isWindowEventAttached = false;
 
-  const visible = (element: HTMLElement) => {
-    let visibility = element.style.visibility;
-    while (visibility === 'inherit') {
-      element = element.parentNode as HTMLElement;
-      visibility = element.style.visibility;
-    }
-    return visibility === 'visible';
-  };
-
-  const focusable = (element: Element, hasTabindex?: boolean) => {
-    let map;
-    let mapName;
-    let img;
-    let focusableIfVisible;
-    let fieldset;
-    let nodeName = element.nodeName.toLowerCase();
-    if (!hasTabindex) {
-      hasTabindex = element.getAttribute(`tabindex`) !== null;
-    }
-
-    if ('area' === nodeName) {
-      map = element.parentNode;
-      mapName = map.name;
-      if (!element.href || !mapName || map.nodeName.toLowerCase() !== 'map') {
-        return false;
-      }
-      img = $("img[usemap='#" + mapName + "']");
-      return img.length > 0 && img.is(':visible');
-    }
-
-    if (/^(input|select|textarea|button|object)$/.test(nodeName)) {
-      focusableIfVisible = !element.disabled;
-
-      if (focusableIfVisible) {
-        // Form controls within a disabled fieldset are disabled.
-        // However, controls within the fieldset's legend do not get disabled.
-        // Since controls generally aren't placed inside legends, we skip
-        // this portion of the check.
-        fieldset = $(element).closest('fieldset')[0];
-        if (fieldset) {
-          focusableIfVisible = !fieldset.disabled;
-        }
-      }
-    } else if ('a' === nodeName) {
-      focusableIfVisible = element.href || hasTabindex;
-    } else {
-      focusableIfVisible = hasTabindex;
-    }
-
-    return (
-      focusableIfVisible && $(element).is(':visible') && visible($(element))
-    );
-  };
-
-  const tabbable = (element: Element) => {
-    const tabIndex = parseInt(element.getAttribute(`tabindex`) || '', 10);
-    const hasTabindex = tabIndex !== null;
-    return (!hasTabindex || tabIndex >= 0) && focusable(element, hasTabindex);
-  };
-
   /**
    * Function to trim whitespaces from a string
    *
